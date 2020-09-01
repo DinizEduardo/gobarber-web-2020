7904750d0032bffb505a50a77a6d48a4c11b86c8
Eduardo Diniz <eduardo.diniz987@gmail.com>
2020-09-01 18:47:10 -0300

    Proximo agendamento no dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { Container, Header, HeaderContent, Profile, Content, Schedule, NextAppointment, Calendar } from './styles';
 
 import logoImg from '../../assets/logo.svg';
-import { FiPower } from 'react-icons/fi';
+import { FiPower, FiClock } from 'react-icons/fi';
 import { useAuth } from '../../hooks/Auth';
 
 const Dashboard: React.FC = () => {
@@ -30,6 +30,33 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horarios agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>06</span>
+            <span>Segunda feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+
+            <div>
+              <img src="https://avatars2.githubusercontent.com/u/52216400?s=460&u=5e790ab37dc071a3fe6ce01fe421647561900932&v=4" alt="Eduardo Diniz" />
+
+              <strong>Eduardo Diniz</strong>
+
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
